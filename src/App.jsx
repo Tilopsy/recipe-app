bcfd1b40f4babe94a2adc3d4e625a078b86d718a
Tilopsy <bettylizzie25@gmail.com>
2024-06-28 09:29:16 +0000

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Welcome from "./pages/Welcome"
+import Categories from "./pages/Categories";
+import Recipes from "./pages/Recipes";
+import Welcome from "./pages/Welcome";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+
+const router = createBrowserRouter([
+  { path: '/', element: <Welcome /> },
+  { path: '/recipes', element: <Recipes /> },
+  { path: '/categories', element: <Categories /> }
+]);
 
 function App() {
   return (
     <>
-     <Welcome />
-    </>
-  )
+      <RouterProvider router={router} />
+      </>
+      )
 }
 
-export default App
+      export default App
